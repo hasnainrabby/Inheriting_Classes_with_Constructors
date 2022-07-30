35f75448af189b0baaf9ce789547dabbb5e5df37
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-07-30 11:38:24 +0600

    toString() method

--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
-class Location{
+class Location{                                                             //All classes inherit the Object class.
  late  num alt,lng;
  //Constructor
 Location(this.alt,this.lng);
@@ -8,12 +8,19 @@ class ElevationLocation extends Location
 {
   num elevation;
   ElevationLocation(num alt, num lng,this.elevation) : super(alt, lng);      //Call Location() Constructor from super/Parent Class.Because Constructor cann't inherit sub class.
-  
+
+@override
+  String toString() {                                                       // override toString() method because of this method using in Object class.
+
+   var result = '$alt,$lng,$elevation';
+    return result;
+  }
 }
 
 main()
 {
    var elevation = new ElevationLocation(240.25, -190.96,250);
-   
-   print('Location = ${elevation.alt},${elevation.lng},${elevation.elevation}');
+
+   print(elevation.toString());
+  // print('Location = ${elevation.alt},${elevation.lng},${elevation.elevation}');
 }
